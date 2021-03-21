9014e62667d152030d002695b405200a5e892662
raul904 <71839824+raul904@users.noreply.github.com>
2021-03-21 19:42:46 +0100

    Update Activitat3.java

--- OneDrive/Documentos/NetBeansProjects/M9UF3/src/m9uf3/Activitat3.java
+++ OneDrive/Documentos/NetBeansProjects/M9UF3/src/m9uf3/Activitat3.java
@@ -9,31 +9,28 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-
 /**
  *
- * @author Raul
+ * @author Raul Yangshun
  */
 public class Activitat3 {
-    	
-	@SuppressWarnings("rawtypes")
 	public static void main (String[] args) {
+            String text;
+            String cerca;
+            int campos;  
+            String linea;
+            boolean contiene = false;
              Scanner scan = new Scanner(System.in);
 		try {           
-			String cerca;
-                        String cadena;
                         
                         System.out.println("Introdueix la url: ");
                         cerca = scan.nextLine();
                         
                         System.out.println("Introdueix el Text: ");
-                        String text;
                         text = scan.nextLine();
                         
-                        System.out.println("Introdueix el numero de camps que vols: ");
-                        int campos=0;                        
+                        System.out.println("Introdueix el numero de camps que vols: ");                      
                         campos = scan.nextInt();
-                        
 
 			URL url = new URL(cerca);
 			URLConnection connexio = url.openConnection();
@@ -46,41 +43,36 @@ public class Activitat3 {
           
 			System.out.println("===============================================================");
                         }
-                   
-                           String headerType = connexio.getContentType();
-                                    String guessType = connexio.guessContentTypeFromName(url.
-                                    getFile());                             
-				System.out.println(cerca);
                         
 			System.out.println("Contingut de [url.getFile()]: " + url.getFile());
 			BufferedReader pagina = new BufferedReader(new InputStreamReader(url.openStream()));
-			
-
-                        Scanner entrada = null;
-                        String linea;
                         int numeroDeLinea = 1;
-                        boolean contiene = false;
+                        contiene = true;
                                 //Leer html
-                                while ((cadena = pagina.readLine()) != null) {
+                                while ((pagina.readLine()) != null) {
                                     linea = pagina.readLine();  //se lee una línea
+                                    if (linea != null) {
                                         if(linea.contains(text)){
+                                             contiene = true;
                                              System.out.println("Linea " + numeroDeLinea + ": " + linea);
                                         }
+                                    }
                                            numeroDeLinea++;
                                 }
-                                if(!contiene){ //si no contienen el texto se muestra un mensaje indicándolo
-
+                                if(contiene==false){ //si no contienen el texto se muestra un mensaje indicándolo
                         System.out.println(text + " no se ha encontrado en el texto");
-                    }
-
-
-
+                    } else {
+                                    System.out.println("Programa acabada");
+                                }
+                   }
+                
+                        catch (MalformedURLException e) {
+                            e.printStackTrace();
                         }
-                        catch (MalformedURLException e) { e.printStackTrace();}
-                        catch (IOException e) {e.printStackTrace();}
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
                 }
-
 }
 
-    
 
