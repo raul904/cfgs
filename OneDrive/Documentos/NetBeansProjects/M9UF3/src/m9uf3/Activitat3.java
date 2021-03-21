b3a830424480160defbab5e351761da1bed21284
raul904 <71839824+raul904@users.noreply.github.com>
2021-03-21 19:54:34 +0100

    Update Activitat3.java

--- OneDrive/Documentos/NetBeansProjects/M9UF3/src/m9uf3/Activitat3.java
+++ OneDrive/Documentos/NetBeansProjects/M9UF3/src/m9uf3/Activitat3.java
@@ -9,45 +9,57 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+
 /**
  *
- * @author Raul Yangshun
+ * @author Raul
  */
 public class Activitat3 {
+
+	@SuppressWarnings("rawtypes")
 	public static void main (String[] args) {
-            String text;
-            String cerca;
-            int campos;  
-            String linea;
-            boolean contiene = false;
              Scanner scan = new Scanner(System.in);
 		try {           
-                        
+			String cerca;
+                        String cadena;
+
                         System.out.println("Introdueix la url: ");
                         cerca = scan.nextLine();
-                        
+
                         System.out.println("Introdueix el Text: ");
+                        String text;
                         text = scan.nextLine();
-                        
-                        System.out.println("Introdueix el numero de camps que vols: ");                      
+
+                        System.out.println("Introdueix el numero de camps que vols: ");
+                        int campos=0;                        
                         campos = scan.nextInt();
+
 
 			URL url = new URL(cerca);
 			URLConnection connexio = url.openConnection();
-			
+
 			//Obtener Campos
 			System.out.println("===============================================================");
 			System.out.println("Camps del 1 al "+campos+" de la Capçalera");
                         for(int i = 1;i<=campos;i++){
 			System.out.println("getHeaderField("+i+")=> " + connexio.getHeaderField(i));
-          
+
 			System.out.println("===============================================================");
                         }
-                        
+
+                           String headerType = connexio.getContentType();
+                                    String guessType = connexio.guessContentTypeFromName(url.
+                                    getFile());                             
+				System.out.println(cerca);
+
 			System.out.println("Contingut de [url.getFile()]: " + url.getFile());
 			BufferedReader pagina = new BufferedReader(new InputStreamReader(url.openStream()));
+
+
+                        Scanner entrada = null;
+                        String linea;
                         int numeroDeLinea = 1;
-                        contiene = true;
+                        boolean contiene = false;
                                 //Leer html
                                 while ((pagina.readLine()) != null) {
                                     linea = pagina.readLine();  //se lee una línea
@@ -62,17 +74,16 @@ public class Activitat3 {
                                 if(contiene==false){ //si no contienen el texto se muestra un mensaje indicándolo
                         System.out.println(text + " no se ha encontrado en el texto");
                     } else {
-                                    System.out.println("Programa acabada");
+                                    System.out.println("Termina el programa");
                                 }
-                   }
-                
-                        catch (MalformedURLException e) {
-                            e.printStackTrace();
+
+
+
                         }
-                        catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        catch (MalformedURLException e) { e.printStackTrace();}
+                        catch (IOException e) {e.printStackTrace();}
                 }
+
 }
 
 
