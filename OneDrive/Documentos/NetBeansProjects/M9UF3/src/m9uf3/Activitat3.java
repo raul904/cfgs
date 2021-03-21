f8be0f146dd77a7b804be13fbfd6896a342a876a
=Raul gimenez <=raulgimenez904@gmail.com>
2021-03-21 18:41:19 +0100

    act terminada m9

---
+++ OneDrive/Documentos/NetBeansProjects/M9UF3/src/m9uf3/Activitat3.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m9uf3;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+
+/**
+ *
+ * @author Raul
+ */
+public class Activitat3 {
+    	
+	@SuppressWarnings("rawtypes")
+	public static void main (String[] args) {
+             Scanner scan = new Scanner(System.in);
+		try {           
+			String cerca;
+                        String cadena;
+                        
+                        System.out.println("Introdueix la url: ");
+                        cerca = scan.nextLine();
+                        
+                        System.out.println("Introdueix el Text: ");
+                        String text;
+                        text = scan.nextLine();
+                        
+                        System.out.println("Introdueix el numero de camps que vols: ");
+                        int campos=0;                        
+                        campos = scan.nextInt();
+                        
+
+			URL url = new URL(cerca);
+			URLConnection connexio = url.openConnection();
+			
+			//Obtener Campos
+			System.out.println("===============================================================");
+			System.out.println("Camps del 1 al "+campos+" de la Capçalera");
+                        for(int i = 1;i<=campos;i++){
+			System.out.println("getHeaderField("+i+")=> " + connexio.getHeaderField(i));
+          
+			System.out.println("===============================================================");
+                        }
+                   
+                           String headerType = connexio.getContentType();
+                                    String guessType = connexio.guessContentTypeFromName(url.
+                                    getFile());                             
+				System.out.println(cerca);
+                        
+			System.out.println("Contingut de [url.getFile()]: " + url.getFile());
+			BufferedReader pagina = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+
+                        Scanner entrada = null;
+                        String linea;
+                        int numeroDeLinea = 1;
+                        boolean contiene = false;
+                                //Leer html
+                                while ((cadena = pagina.readLine()) != null) {
+                                    linea = pagina.readLine();  //se lee una línea
+                                        if(linea.contains(text)){
+                                             System.out.println("Linea " + numeroDeLinea + ": " + linea);
+                                        }
+                                           numeroDeLinea++;
+                                }
+                                if(!contiene){ //si no contienen el texto se muestra un mensaje indicándolo
+
+                        System.out.println(text + " no se ha encontrado en el texto");
+                    }
+
+
+
+                        }
+                        catch (MalformedURLException e) { e.printStackTrace();}
+                        catch (IOException e) {e.printStackTrace();}
+                }
+
+}
+
+    
+
